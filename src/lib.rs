dc56a5e17f3939ecb8025529a541b59af7c72640
Pierre-Yves Aillet <pyaillet@gmail.com>
2022-02-19 11:52:47 +0100

    :recycle: rework error handling

--- src/lib.rs
+++ src/lib.rs
@@ -37,9 +37,9 @@ pub enum State {
     Initialized(ChipId),
 }
 
-impl<I2C> Bma423<I2C>
+impl<I2C, E> Bma423<I2C>
 where
-    I2C: Write + WriteRead,
+    I2C: Write<Error = E> + WriteRead<Error = E>,
 {
     pub fn new(i2c: I2C) -> Self {
         Self {
@@ -57,15 +57,14 @@ where
         }
     }
 
-    fn probe_chip(&mut self) -> Result<u8, Bma423Error> {
+    fn probe_chip(&mut self) -> Result<u8, E> {
         let mut data: [u8; 1] = [0; 1];
         self.i2c
-            .write_read(self.address, &[Reg::ChipId as u8], &mut data)
-            .map_err(|_| Bma423Error::I2cError)?;
+            .write_read(self.address, &[Reg::ChipId as u8], &mut data)?;
         Ok(data[0])
     }
 
-    pub fn init(&mut self) -> Result<(), Bma423Error> {
+    pub fn init(&mut self) -> Result<(), E> {
         let chip_id = self.probe_chip()?;
         self.state = State::Initialized(chip_id.into());
         Ok(())
@@ -77,11 +76,10 @@ where
             State::Initialized(chip_id) => Ok(chip_id),
         }
     }
-    pub fn get_status(&mut self) -> Result<u8, Bma423Error> {
+    pub fn get_status(&mut self) -> Result<u8, E> {
         let mut data: [u8; 1] = [0; 1];
         self.i2c
-            .write_read(self.address, &[Reg::Status as u8], &mut data)
-            .map_err(|_| Bma423Error::I2cError)?;
+            .write_read(self.address, &[Reg::Status as u8], &mut data)?;
         Ok(data[0])
     }
 }
