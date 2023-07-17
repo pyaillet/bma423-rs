c972aec8ba19d723b29208ec86f0eb17f449f756
Dan Whitman <dwhitman44@mgail.com>
2023-07-16 23:23:24 -0400

    Just backing up some work. Added a soft reset in the init method so the chip returns to a known state and the config file can be loaded again for sure. Waits for the internal status to be ok after completing loading the config file to ensure that it was successful, with a timeout. Sets up the accelerometer into a known configuration at the end of init so it is usable right away, considering adding some type states instead. Currently full of some test methods written to get the any motion and tap detection features working.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #[cfg(feature = "accel")]
 use accelerometer::{vector::F32x3, Accelerometer};
 
+use derive_new::new;
 use embedded_hal::blocking::{
     delay::DelayUs,
     i2c::{Write, WriteRead},
@@ -30,6 +31,8 @@ enum Reg {
 
     FeatureInterruptStatus = 0x1c,
     HardwareInterruptStatus = 0x1d,
+
+    InternalStatus = 0x2a,
 
     AccelConfig = 0x40,
     AccelRange = 0x41,
@@ -100,6 +103,7 @@ pub enum InterruptLine {
 /// Features of the accelerometer
 #[bitmask(u8)]
 #[derive(Copy, Clone, Debug)]
+// TODO: Need to add any/no motion features
 pub enum Features {
     StepCounter = Self(0b0000_0001),
     StepActivity = Self(0b0000_0010),
@@ -111,6 +115,7 @@ pub enum Features {
 #[repr(u8)]
 #[derive(Copy, Clone, Debug)]
 pub enum FeatureOffset {
+    // TODO: These are incorrect per the data sheet.
     AnyMotion = 0x00,
     NoMotion = 0x04,
     StepCounterParam = 0x08,
@@ -210,10 +215,20 @@ pub enum AccelRange {
     Range8g = 0x02,
     Range16g = 0x03,
 }
+impl AccelRange {
+    pub fn as_float(&self) -> f32 {
+        match self {
+            AccelRange::Range2g => 2.0,
+            AccelRange::Range4g => 4.0,
+            AccelRange::Range8g => 8.0,
+            AccelRange::Range16g => 16.0,
+        }
+    }
+}
 
-const DEFAULT_ADDRESS: u8 = 0x19;
+const DEFAULT_ADDRESS: u8 = 0x18;
+const FEATURE_SIZE: usize = 0x46;
 const READ_WRITE_LEN: usize = 0x08;
-const FEATURE_SIZE: usize = 0x70;
 const GRAVITY_EARTH: f32 = 9.80665;
 
 #[derive(Clone, Copy, Debug)]
@@ -221,6 +236,7 @@ pub enum Error<E> {
     BusError(E),
     I2cError,
     ConfigError,
+    BadInternal(u8),
     Uninitialized,
 }
 
@@ -240,20 +256,40 @@ pub enum ChipId {
 
 /// Structure representing the Bma423 device
 pub struct Bma423<I2C> {
+    /// I2C address
     address: u8,
+    /// I2C peripheral
     i2c: I2C,
+    /// Current state
     state: State,
-    config: Option<Config>,
+    /// Current configuration
+    config: Config,
 }
 
 /// Configuration of the device
 #[allow(dead_code)]
-#[derive(Copy, Clone, Debug)]
+#[derive(new, Copy, Clone, Debug)]
 pub struct Config {
-    bandwidth: AccelConfigBandwidth,
-    range: AccelRange,
-    performance_mode: AccelConfigPerfMode,
-    sample_rate: AccelConfigOdr,
+    pub bandwidth: AccelConfigBandwidth,
+    pub range: AccelRange,
+    pub performance_mode: AccelConfigPerfMode,
+    pub sample_rate: AccelConfigOdr,
+}
+impl Default for Config {
+    /// The default configuration is:
+    /// * Filter: Average of 4 samples
+    /// * Range: 2g
+    /// * Performance mode: Averaging
+    /// * Sample rate: 50 Hz
+    fn default() -> Self {
+        Self {
+            // TODO: Keep these? If so change doc comment
+            bandwidth: AccelConfigBandwidth::NormAvg4,
+            range: AccelRange::Range2g,
+            performance_mode: AccelConfigPerfMode::CicAvg,
+            sample_rate: AccelConfigOdr::Odr50,
+        }
+    }
 }
 
 pub enum State {
@@ -265,7 +301,7 @@ impl<E, I2C> Bma423<I2C>
 where
     I2C: Write<Error = E> + WriteRead<Error = E>,
 {
-    /// Create a new Bma423 device with the default slave address
+    /// Create a new Bma423 device with the default slave address (0x18) and configuration.
     ///
     /// # Arguments
     ///
@@ -275,16 +311,13 @@ where
     ///
     /// - [Bma423 driver](Bma423) created
     ///
+    #[inline(always)]
     pub fn new(i2c: I2C) -> Self {
-        Self {
-            address: DEFAULT_ADDRESS,
-            i2c,
-            state: State::Uninitialized,
-            config: None,
-        }
+        Self::new_with_address(i2c, DEFAULT_ADDRESS)
     }
 
-    /// Create a new Bma423 device with the default slave address
+    /// Create a new Bma423 device with a particular slave address and default
+    /// configuration.
     ///
     /// # Arguments
     ///
@@ -300,7 +333,7 @@ where
             address,
             i2c,
             state: State::Uninitialized,
-            config: None,
+            config: Config::default(),
         }
     }
 
@@ -312,6 +345,13 @@ where
     fn write_read(&mut self, reg: Reg, data: &mut [u8]) -> Result<(), Error<E>> {
         self.i2c.write_read(self.address, &[reg.into()], data)?;
         Ok(())
+    }
+
+    /// Reads only a single byte from a register
+    fn read_register(&mut self, reg: Reg) -> Result<u8, Error<E>> {
+        let mut data = [0; 1];
+        self.write_read(reg, &mut data)?;
+        Ok(data[0])
     }
 
     fn probe_chip(&mut self) -> Result<u8, Error<E>> {
@@ -339,17 +379,50 @@ where
         let chip_id = self.probe_chip()?;
         self.state = State::Initialized(chip_id.into());
 
+        // First, perform a soft reset followed by an arbitrary delay
+        self.write(&[Reg::Command.into(), 0xb6])?;
+        delay.delay_us(1000);
+
+        // Disable advanced power saving mode
         self.set_power_config(PowerConfigurationFlag::none())?;
 
+        // Wait a bit
         delay.delay_us(500);
 
-        self.write(&[Reg::StartInitialization.into(), 0u8])?;
+        // Enter config file writing mode
+        self.write(&[Reg::StartInitialization.into(), 0])?;
 
+        // TODO: Temporarily just write all zeros to disabled everything
+        // while we try to get something working.
+        /* let mut data = [0; FEATURE_SIZE + 1];
+        data[0] = Reg::FeatureConfig.into();
+        // Correct default axes mapping
+        //data[10 + 1] = 0x12;
+        //data[0x3E + 1] = 0x88;
+        self.write(&data)?; */
+
+        // Stream write the config file
         self.stream_write(Reg::FeatureConfig, &config::BMA423_CONFIG_FILE)?;
 
+        // Exit config file writing mode
         self.write(&[Reg::StartInitialization.into(), 1u8])?;
 
+        // Wait until the chip is ready and initialized with timeout
+        let mut time_ms: usize = 500;
+        while time_ms > 0 && (self.read_register(Reg::InternalStatus)? & 0x0F) != 0x01 {
+            delay.delay_us(1000);
+            time_ms -= 1;
+        }
+        if time_ms == 0 {
+            // We timed out, so there's a serious problem
+            return Err(Error::BadInternal(self.read_register(Reg::InternalStatus)?));
+        }
+
+        // Enable accelerometer power
         self.set_power_control(PowerControlFlag::Accelerometer)?;
+
+        // Set the configuration to the default
+        self.set_accel_config(self.config)?;
 
         Ok(())
     }
@@ -379,36 +452,26 @@ where
         Ok(())
     }
 
-    pub fn set_accel_config(
-        &mut self,
-        odr: AccelConfigOdr,
-        bw: AccelConfigBandwidth,
-        rm: AccelConfigPerfMode,
-        g_range: AccelRange,
-    ) -> Result<(), Error<E>> {
-        if rm == AccelConfigPerfMode::Continuous {
-            if (bw as u8) > (AccelConfigBandwidth::NormAvg4 as u8) {
+    pub fn set_accel_config(&mut self, config: Config) -> Result<(), Error<E>> {
+        if config.performance_mode == AccelConfigPerfMode::Continuous {
+            if (config.bandwidth as u8) > (AccelConfigBandwidth::NormAvg4 as u8) {
                 return Err(Error::ConfigError);
             }
-        } else if rm == AccelConfigPerfMode::CicAvg {
-            if (bw as u8) > (AccelConfigBandwidth::ResAvg128 as u8) {
+        } else if config.performance_mode == AccelConfigPerfMode::CicAvg {
+            if (config.bandwidth as u8) > (AccelConfigBandwidth::ResAvg128 as u8) {
                 return Err(Error::ConfigError);
             }
         } else {
             return Err(Error::ConfigError);
         }
 
-        let accel_config: u8 = odr as u8 | bw as u8 | rm as u8;
-        let accel_range: u8 = g_range as u8;
+        let accel_config: u8 =
+            config.sample_rate as u8 | config.bandwidth as u8 | config.performance_mode as u8;
+        let accel_range: u8 = config.range as u8;
         self.write(&[Reg::AccelConfig.into(), accel_config])?;
         self.write(&[Reg::AccelRange.into(), accel_range])?;
 
-        self.config = Some(Config {
-            sample_rate: odr,
-            bandwidth: bw,
-            performance_mode: rm,
-            range: g_range,
-        });
+        self.config = config;
         Ok(())
     }
 
@@ -478,6 +541,81 @@ where
         Ok(())
     }
 
+    pub fn get_features_mem(&mut self) -> Result<[u8; FEATURE_SIZE], Error<E>> {
+        // TODO: Get FEATURE_IN memory
+        let mut feature_config: [u8; FEATURE_SIZE] = [0; FEATURE_SIZE];
+        self.write_read(Reg::FeatureConfig, &mut feature_config)?;
+        Ok(feature_config)
+    }
+
+    // TODO: Make this permanent? If kept may need to abstract error
+    pub fn get_error(&mut self) -> Result<u8, Error<E>> {
+        self.read_register(Reg::Error)
+    }
+
+    // TODO: Temporary? If kept may need to abstract status
+    pub fn get_internal_status(&mut self) -> Result<u8, Error<E>> {
+        self.read_register(Reg::InternalStatus)
+    }
+
+    // TODO: Temporary? If kept may need to abstract error
+    pub fn get_internal_error(&mut self) -> Result<u8, Error<E>> {
+        self.read_register(Reg::InternalError)
+    }
+
+    // TODO: Test function to see if we can get feature interrupts to work at all
+    pub fn setup_any_motion_interrupt(&mut self) -> Result<(), Error<E>> {
+        // Enable any motion feature for all axes
+        // Assumes little endian
+        let feature_config = [Reg::FeatureConfig.into(), 0xAA, 0x00, 0x05, 0b1110_0000];
+        self.write(&feature_config)?;
+
+        // Set interrupt pin behavior in INT1_IO_CTRL
+        self.write(&[Reg::Interrupt1IOCtl.into(), 0b0000_1010])?;
+
+        // Setup interrupt latching in INT_LATCH
+        self.write(&[Reg::InterruptConfig.into(), 0b0000_0001])?;
+
+        // Map feature to interrupt in INT1_MAP
+        // Note that this driver does currently support this but it's functioning has not been verified
+        self.write(&[Reg::FeatureInterrupt1Mapping.into(), 0b0010_0000])?;
+
+        Ok(())
+    }
+
+    // TODO
+    pub fn setup_tap_detection_interrupt(&mut self) -> Result<(), Error<E>> {
+        // TODO: Note that this does not seem to work correctly
+        //self.enable_feature(Features::SingleTap)?;
+
+        // So we do it manually for now
+        let mut feature_config = [0; FEATURE_SIZE + 1];
+        self.write_read(Reg::FeatureConfig, &mut feature_config[1..])?;
+        feature_config[0] = Reg::FeatureConfig.into();
+        feature_config[0x3C + 1] |= 0x01;
+        self.write(&feature_config)?;
+
+        // Set interrupt pin behavior in INT2_IO_CTRL
+        self.write(&[Reg::Interrupt2IOCtl.into(), 0b0000_1010])?;
+
+        // Setup interrupt latching in INT_LATCH
+        self.write(&[Reg::InterruptConfig.into(), 0b0000_0001])?;
+
+        // Map feature to interrupt in INT2_MAP
+        // Note that this driver does currently support this but it's functioning has not been verified
+        self.write(&[Reg::FeatureInterrupt2Mapping.into(), 0b0000_0001])?;
+
+        Ok(())
+    }
+
+    // TODO delete probably
+    pub fn test_regs(&mut self) -> Result<[u8; 3], Error<E>> {
+        let a = self.read_register(Reg::Interrupt1IOCtl)?;
+        let b = self.read_register(Reg::InterruptConfig)?;
+        let c = self.read_register(Reg::FeatureInterrupt1Mapping)?;
+        Ok([a, b, c])
+    }
+
     pub fn map_feature_interrupt(
         &mut self,
         line: InterruptLine,
@@ -537,12 +675,8 @@ where
         Ok(data[0])
     }
 
-    /// Get x, y, z acceleration
-    ///
-    /// # Returns:
-    ///
-    /// - (f32, f32, f32), represeting the acceleration values for (x, y, z) axis
-    pub fn get_x_y_z(&mut self) -> Result<(f32, f32, f32), Error<E>> {
+    /// Returns the normalized accelerations in g.
+    fn accel_norm_int(&mut self) -> Result<(f32, f32, f32), Error<E>> {
         let mut data: [u8; 6] = [0; 6];
         self.write_read(Reg::AccXLSB, &mut data)?;
 
@@ -550,10 +684,23 @@ where
         let y: i16 = ((((data[3] as i16) << 8) as i16) | (data[2] as i16)) / 0x10;
         let z: i16 = ((((data[5] as i16) << 8) as i16) | (data[4] as i16)) / 0x10;
 
+        let range = self.config.range.as_float();
+
         Ok((
-            lsb_to_ms2(x, 2.0, 12),
-            lsb_to_ms2(y, 2.0, 12),
-            lsb_to_ms2(z, 2.0, 12),
+            lsb_to_ms2(x, range, 12),
+            lsb_to_ms2(y, range, 12),
+            lsb_to_ms2(z, range, 12),
+        ))
+    }
+
+    /// Returns the current x, y, z accelerations in absolute units of m/s^2.
+    pub fn accel_abs(&mut self) -> Result<(f32, f32, f32), Error<E>> {
+        let accel = self.accel_norm_int()?;
+
+        Ok((
+            GRAVITY_EARTH * accel.0,
+            GRAVITY_EARTH * accel.1,
+            GRAVITY_EARTH * accel.2,
         ))
     }
 }
@@ -567,27 +714,23 @@ where
     type Error = Error<E>;
 
     fn accel_norm(&mut self) -> Result<F32x3, accelerometer::Error<Error<E>>> {
-        let (x, y, z) = self.get_x_y_z()?;
-        Ok(F32x3::new(x, y, z))
+        Ok(self.accel_norm_int()?.into())
     }
 
     fn sample_rate(&mut self) -> Result<f32, accelerometer::Error<Error<E>>> {
-        match self.config {
-            Some(config) => match config.sample_rate {
-                AccelConfigOdr::Odr0p78 => Ok(25.0 / 32.0),
-                AccelConfigOdr::Odr1p5 => Ok(25.0 / 16.0),
-                AccelConfigOdr::Odr3p1 => Ok(25.0 / 8.0),
-                AccelConfigOdr::Odr6p25 => Ok(25.0 / 4.0),
-                AccelConfigOdr::Odr12p5 => Ok(25.0 / 2.0),
-                AccelConfigOdr::Odr25 => Ok(25.0),
-                AccelConfigOdr::Odr50 => Ok(50.0),
-                AccelConfigOdr::Odr100 => Ok(100.0),
-                AccelConfigOdr::Odr200 => Ok(200.0),
-                AccelConfigOdr::Odr400 => Ok(400.0),
-                AccelConfigOdr::Odr800 => Ok(800.0),
-                AccelConfigOdr::Odr1k6 => Ok(1600.0),
-            },
-            None => Err(accelerometer::Error::from(Error::ConfigError)),
+        match self.config.sample_rate {
+            AccelConfigOdr::Odr0p78 => Ok(25.0 / 32.0),
+            AccelConfigOdr::Odr1p5 => Ok(25.0 / 16.0),
+            AccelConfigOdr::Odr3p1 => Ok(25.0 / 8.0),
+            AccelConfigOdr::Odr6p25 => Ok(25.0 / 4.0),
+            AccelConfigOdr::Odr12p5 => Ok(25.0 / 2.0),
+            AccelConfigOdr::Odr25 => Ok(25.0),
+            AccelConfigOdr::Odr50 => Ok(50.0),
+            AccelConfigOdr::Odr100 => Ok(100.0),
+            AccelConfigOdr::Odr200 => Ok(200.0),
+            AccelConfigOdr::Odr400 => Ok(400.0),
+            AccelConfigOdr::Odr800 => Ok(800.0),
+            AccelConfigOdr::Odr1k6 => Ok(1600.0),
         }
     }
 }
@@ -596,5 +739,5 @@ where
 fn lsb_to_ms2(val: i16, g_range: f32, bit_width: u8) -> f32 {
     let half_scale: f32 = (1 << bit_width) as f32 / 2.0;
 
-    GRAVITY_EARTH * val as f32 * g_range / half_scale
+    val as f32 * g_range / half_scale
 }
